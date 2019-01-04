5ac1fb349ce8f369bf87b45501c0da3e99818385
Thanapon <thanapon_team@hotmail.com>
2019-01-04 17:55:51 +0700

    The First React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      products: "",
+      status2: {},
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://192.168.4.153:8000/api/products')
+      .then((res) => {
+        console.log(res);
+       
+        this.setState({ products: res.data, status2: res.headers })
+        console.log(this.state.status2)
+      })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.products && this.state.products.map(element => {
+          return (<div key={element.id}>
+            <h2>{element.productName}</h2>
+            <p>{element.unitPrice}</p>
+          </div>)
+        })}
       </div>
     );
   }
